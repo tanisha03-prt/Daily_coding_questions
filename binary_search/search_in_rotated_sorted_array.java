549033a9461d7ec17f819f83042d8063d58c93fc
Tanisha <tanishasingh0302@gmail.com>
2026-07-02 19:31:13 +0530

    Added seacrh in rotated sorted array

---
+++ binary_search/search_in_rotated_sorted_array.java
@@ -0,0 +1,26 @@
+package binary_search;
+import java.util.*;
+
+public class search_in_rotated_sorted_array {
+    public int search(int[] arr, int n, int target){
+        int low = 0;
+        int high = n-1;
+        while(low < high){
+            int mid = (low+high)/2;
+            if(arr[low] <= arr[mid]){
+                if(arr[mid] >= target && arr[low] <= target){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(arr[mid] <= target && arr[high] >= target){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}

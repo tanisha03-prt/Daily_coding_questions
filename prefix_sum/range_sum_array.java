89c56007f71db378083816101a7d4e9dcc59e780
Tanisha <tanishasingh0302@gmail.com>
2026-07-09 00:30:25 +0530

    Range sum query

--- prefix_sum/range_sum_array.java
+++ prefix_sum/range_sum_array.java
@@ -1,11 +1,17 @@
 package prefix_sum;
-import java.util.*;
+
 public class range_sum_array {
-    public static void main(String[] args) {
-        int[] arr = {1,2,5,34,6,4,7};
-        int l = arr[1];
-        int r = arr[4];
-        
+    public int[] pre;
+    public range_sum_array(int[] nums) {
+        int n = nums.length;
+        pre = new int[n];
+        pre[0] = nums[0];
+        for(int i=1;i<n;i++){
+            pre[i] = pre[i-1] + nums[i]; 
+        }
     }
-    
+    public int sumRange(int left, int right) {
+        if(left == 0) return pre[right];
+        return pre[right] - pre[left-1];
+    }
 }

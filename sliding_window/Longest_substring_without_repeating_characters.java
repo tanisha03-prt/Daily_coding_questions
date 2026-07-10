b43f48ee2cecaed2f13155bc44ae934908029af5
Tanisha <tanishasingh0302@gmail.com>
2026-07-10 22:04:11 +0530

    Longest_substring_without_repeating_characters

---
+++ sliding_window/Longest_substring_without_repeating_characters.java
@@ -0,0 +1,19 @@
+package sliding_window;
+import java.util.*;
+
+public class Longest_substring_without_repeating_characters {
+    public int len(String s){
+        Map<Character,Integer> map = new HashMap<>();
+        int left = 0;
+        int maxLen = 0;
+        for(int right = 0; right<s.length(); right++){
+            char ch = s.charAt(right);
+            if(map.containsKey(ch)){
+                left = Math.max(left,map.get(ch) + 1);
+            }
+            map.put(ch,right);
+            maxLen = Math.max(maxLen,right - left + 1);
+        }
+        return maxLen;
+    }
+}

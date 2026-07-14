f409cd953c3de67028b7faceb4cd156237dbf373
Tanisha <tanishasingh0302@gmail.com>
2026-07-14 21:33:55 +0530

    Boats to Save People

---
+++ greedy/boats_to_save_people.java
@@ -0,0 +1,23 @@
+package greedy;
+
+import java.util.Arrays;
+
+public class boats_to_save_people {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int count = 0;
+        while(left <= right){
+            if(people[left] + people[right] <= limit){
+                count++;
+                left++;
+                right--;
+            }else{
+                count++;
+                right--;
+            }
+        }
+        return count;
+    }
+}

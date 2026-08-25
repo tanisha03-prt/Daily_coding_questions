369746357b01e5488869a8cf2b251235989ae75a
Tanisha <tanishasingh0302@gmail.com>
2026-08-26 04:20:58 +0530

    Bag of tokens score

---
+++ greedy/bagOfTokens.java
@@ -0,0 +1,29 @@
+package greedy;
+
+import java.util.Arrays;
+
+public class bagOfTokens {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        int n = tokens.length;
+        int maxScore = 0;
+        Arrays.sort(tokens);
+        int i = 0;
+        int j = n - 1;
+        int score = 0;
+        while (i <= j) {
+            if (power >= tokens[i]) {
+                power -= tokens[i];
+                score++;
+                i++;
+                maxScore = Math.max(maxScore, score);
+            } else if (score >= 1) {
+                power += tokens[j];
+                score--;
+                j--;
+            } else {
+                return maxScore;
+            }
+        }
+        return maxScore;
+    }
+}

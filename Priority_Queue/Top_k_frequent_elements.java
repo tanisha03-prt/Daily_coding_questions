06e8ca1f27d692693078609560c2e8c460cc39e6
Tanisha <tanishasingh0302@gmail.com>
2026-09-13 14:26:29 +0530

    Top K Frequent ELements

---
+++ Priority_Queue/Top_k_frequent_elements.java
@@ -0,0 +1,21 @@
+package Priority_Queue;
+
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class Top_k_frequent_elements {
+    public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+        pq.addAll(map.keySet());
+        int[] res = new int[k];
+        for(int i=0;i<k;i++){
+            res[i] = pq.poll();
+        }
+        return res;
+    }
+}

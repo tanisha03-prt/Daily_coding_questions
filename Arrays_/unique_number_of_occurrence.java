9a8fa94072bae3b76f804423f0a1c22d3fc2ebd8
Tanisha <tanishasingh0302@gmail.com>
2026-09-13 14:18:44 +0530

    Unique number of occurrences

---
+++ Arrays_/unique_number_of_occurrence.java
@@ -0,0 +1,21 @@
+package Arrays_;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class unique_number_of_occurrence {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer>map= new HashMap<>();
+        for(int num : arr){
+            map.put(num, map.getOrDefault(num,0) + 1);
+        }
+        HashSet<Integer>set = new HashSet<>();
+        for(int freq : map.values()){
+            if(set.contains(freq)){
+                return false;
+            }
+            set.add(freq);
+        }
+        return true;
+    }
+}

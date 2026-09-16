2dc8af49ed7785efb38c1412bbc301797072c377
Tanisha <tanishasingh0302@gmail.com>
2026-09-16 13:38:03 +0530

    Two sum

---
+++ hash_table/two_sum.java
@@ -0,0 +1,15 @@
+package hash_table;
+import java.util.*;
+public class two_sum {
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int req = target - nums[i];
+            if(map.containsKey(req)){
+                return new int[]{map.get(req),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    } 
+}

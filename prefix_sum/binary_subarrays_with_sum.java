1711d1510155fc98d5221999c296025644a7204c
Tanisha <tanishasingh0302@gmail.com>
2026-09-24 10:45:23 +0530

    no of subarrays with sum goal

---
+++ prefix_sum/binary_subarrays_with_sum.java
@@ -0,0 +1,21 @@
+package prefix_sum;
+
+import java.util.HashMap;
+
+public class binary_subarrays_with_sum {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int n = nums.length;
+        int sum = 0;
+        int count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(map.containsKey(sum-goal)){
+                count+=map.get(sum-goal);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}

fc8430da52a04ef661ffaa20a7286f8171258e1c
Tanisha <tanishasingh0302@gmail.com>
2026-09-25 01:50:32 +0530

    Word Pattern

---
+++ hash_table/word_pattern.java
@@ -0,0 +1,27 @@
+package hash_table;
+
+import java.util.HashMap;
+
+public class word_pattern {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if(pattern.length() != words.length) {
+            return false;
+        }
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String, Character> map2 = new HashMap<>();
+        for(int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+            if(map1.containsKey(ch) && !map1.get(ch).equals(word)) {
+                return false;
+            }
+            if(map2.containsKey(word) && map2.get(word) != ch) {
+                return false;
+            }
+            map1.put(ch, word);
+            map2.put(word, ch);
+        }
+        return true;
+    }
+}

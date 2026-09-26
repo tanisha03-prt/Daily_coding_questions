b2bb0fe052622f00603160294c8de30980d228b8
Tanisha <tanishasingh0302@gmail.com>
2026-09-26 14:30:26 +0530

    Non overlapping intervals

--- greedy/non_overlapping_intervals.java
+++ greedy/non_overlapping_intervals.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 
 public class non_overlapping_intervals {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->{
-            return a[1]-b[1];});
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
         int res=0;
         int end=intervals[0][1];        
         for(int i=1;i<intervals.length;i++){

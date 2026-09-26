680d5036d7a3d83ed8d0e996f80da99aafd5dfdf
Tanisha <tanishasingh0302@gmail.com>
2026-09-26 13:51:54 +0530

    Two city scheduling

---
+++ greedy/two_city_scheduling.java
@@ -0,0 +1,18 @@
+package greedy;
+
+import java.util.Arrays;
+
+public class two_city_scheduling {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, (a, b) -> (a[0] - a[1]) - (b[0] - b[1]));
+        int n = costs.length / 2;
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += costs[i][0];
+        }
+        for (int i = n; i < costs.length; i++) {
+            total += costs[i][1];
+        }
+        return total;
+    }
+}

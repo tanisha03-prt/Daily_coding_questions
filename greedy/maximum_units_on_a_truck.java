73216100ab81f235cd90d5da2a72255fe425e4d0
Tanisha <tanishasingh0302@gmail.com>
2026-09-26 11:37:06 +0530

    revised maximum units on truck

--- greedy/maximum_units_on_a_truck.java
+++ greedy/maximum_units_on_a_truck.java
@@ -3,17 +3,17 @@ package greedy;
 import java.util.Arrays;
 
 public class maximum_units_on_a_truck {
-    public int maxUnits(int[][] boxTypes, int truckSize){
-        Arrays.sort(boxTypes,(a,b) -> b[1] - a[1]);
-        int units = 0;
-        for(int i=0; i<truckSize.length; i++){
-            int boxes = Math.min(boxTypes[i][0],truckSize);
-            units += boxes * boxTypes[i][1];
-            truckSize -= boxes;
-            if (truckSize == 0) {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes,(a,b)->b[1] - a[1]);
+        int total = 0;
+        for(int[] box : boxTypes){
+            int take = Math.min(box[0],truckSize);
+            total += take*box[1];
+            truckSize -= take;
+            if(truckSize == 0){
                 break;
             }
         }
-        return units;
+        return total;
     }
 }
